05e54cb61e11c4edb2459a2f3865b187923cd7d0
cahito <cahito@gmail.com>
2022-07-29 18:00:03 -0300

    Feat - Requisito um cumprido

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import ProductsRoutes from './routes/products.routes';
+import 'express-async-errors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(ProductsRoutes);
+
+app.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
+  const { name, message } = err;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
+});
 
 export default app;
---
+++ src/services/products.service.ts
@@ -0,0 +1,15 @@
+import connection from '../models/connection';
+import ProductsModel from '../models/products.model';
+import Product from '../interfaces/products.interface';
+
+export default class ProductsService {
+  public model: ProductsModel;
+
+  constructor() {
+    this.model = new ProductsModel(connection);
+  }
+
+  public create(product: Product): Promise<Product> {
+    return this.model.create(product);
+  }
+}
